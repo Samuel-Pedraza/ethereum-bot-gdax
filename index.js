4184fc70eb8368e68440d29950806ae53abde4ed
sam <samdpedraza@gmail.com>
2018-02-12 20:21:19 -0800

    completed api request, now can begin business logic

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var gdax = require("gdax");
 var publicClient = new gdax.PublicClient();
 
-publicClient.getProductHistoricRates('ETH-USD', { granularity: 3600 }, function(error, data){
-	console.log(typeof(data.body))
+publicClient.getProductHistoricRates('ETH-USD', { granularity: 3600 }, function(error, response, data){
+	if(error){
+		console.log("error");
+	} else {
+		var retrievedData = 0;
+		for(var i = 0; i < data.length; i++){
+			retrievedData += data[i][4];
+		}
+		console.log(retrievedData);
+		console.log(i);
+		console.log(retrievedData / i);
+	}
 })
 
