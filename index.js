eed35c07c6a674f19bd278249b20497ab8f69f43
sam <samdpedraza@gmail.com>
2018-02-12 21:05:55 -0800

    finished making basic buying bot for ethereum

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 var gdax = require("gdax");
 var publicClient = new gdax.PublicClient();
 
-publicClient.getProductHistoricRates('ETH-USD', { granularity: 3600 }, function(error, response, data){
-	if(error){
-		console.log("error");
-	} else {
-		var retrievedData = 0;
-		for(var i = 0; i < data.length; i++){
-			retrievedData += data[i][4];
-		}
-		console.log(retrievedData);
-		console.log(i);
-		console.log(retrievedData / i);
-	}
-})
+var bankAccount = 100;
+var purchased = false;
+var ethHolding = 0;
 
+function getEth(){
+
+	publicClient.getProductHistoricRates('ETH-USD', { granularity: 60 }, function(error, response, data){
+		if(error){
+			console.log("error");
+		} else {
+			var retrievedData = 0;
+			for(var i = 0; i < data.length; i++){
+				retrievedData += data[i][4];
+			}
+			var average_price = retrievedData / i;
+			var current_price = data[0][4];
+			if(current_price < average_price - (average_price * 0.025) && !purchased && bankAccount > 0){
+				ethHolding = bankAccount / current_price;
+				purchased = true;
+				console.log("bought");
+				console.log("Current Price:" + current_price);
+				console.log("Average Price: " + (average_price - (average_price * 0.025)).toString());
+				console.log("Ethereum Holding: " + ethHolding);
+				console.log("Bank Account: " + bankAccount);
+			}
+			else if(purchased && current_price > average_price){
+				money_made = ethHolding * current_price;
+				bankAccount += money_made;
+				purchased = false;
+				console.log("sold");
+				console.log("Current Price:" + current_price);
+				console.log("Average Price: " + (average_price - (average_price * 0.025)).toString());
+				console.log("Ethereum Holding: " + ethHolding);
+				console.log("Bank Account: " + bankAccount);
+			}
+		 	else {
+				console.log("not purchased");
+				console.log("Current Price:" + current_price);
+				console.log("Average Price: " + (average_price - (average_price * 0.025)).toString());
+				console.log("Ethereum Holding: " + ethHolding);
+				console.log("Bank Account: " + bankAccount);
+			}
+		}
+	})
+}
+
+setInterval(getEth, 5000);
