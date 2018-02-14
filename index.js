7e0e683a552dc4835bfd0861f178cf9672c372a4
sam <samdpedraza@gmail.com>
2018-02-14 09:13:56 -0800

    fixed

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var bankAccount = 100;
 var purchased = false;
 var ethHolding = 0;
 var purchasedAtEMA = 0;
+var buys = 0;
+var sells = 0;
 
 function calculateSMA(data){
 	var holderSMA = 0;
@@ -49,31 +51,37 @@ function getEth(){
 			var average_price = retrievedData / i;
 			var current_price = data[0][4];
 
-			if(current_price < calculateEMA(data) && !purchased && bankAccount > 0){
+			if(current_price < (calculateEMA(data)) && !purchased && bankAccount > 0){
 				ethHolding = bankAccount / current_price;
 				purchased = true;
 				bankAccount = 0;
 				purchasedAtEMA = calculateEMA(data);
-				
+				buys += 1;
+
 				console.log("bought");
 				console.log("EMA: " + calculateEMA(data))
 				console.log("Current Price: " + current_price);
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
+				console.log("Buys: " + buy);
 				console.log("");
+
 			}
 
-			else if(purchased && current_price > purchasedAtEMA){
+			else if(purchased && (current_price > purchasedAtEMA) || (current_price === (purchasedAtEMA - (purchasedAtEMA * 0.015)))){
 				money_made = ethHolding * current_price;
 				bankAccount += money_made;
 				purchased = false;
 				ethHolding = 0;
+				purchasedAtEMA = 0;
+				sell +=1;
 
 				console.log("sold");
 				console.log("EMA: " + calculateEMA(data))
 				console.log("Current Price: " + current_price);
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
+				console.log("Sells: " + sell);
 				console.log("");
 			}
 
@@ -83,6 +91,11 @@ function getEth(){
 				console.log("Current Price: " + current_price);
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
+				
+				if( purchasedAtEMA > 0){
+					console.log("purchasedAtEMA: " + purchasedAtEMA);
+				}
+
 				console.log("");
 			}
 		}
@@ -91,3 +104,11 @@ function getEth(){
 
 
 setInterval(getEth, 5000);
+
+//track number of trades
+//track return on trades w vs l
+//track hold time of trade
+//track average hold time
+
+//keep track of high
+//buy only when EMA - (ema * 0.01) >= current price
