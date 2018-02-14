98b1ede1d16fad4d41d813d4a62490bc94635917
sam <samdpedraza@gmail.com>
2018-02-13 16:41:39 -0800

    Added ability to calculate SMA

--- index.js
+++ index.js
@@ -5,40 +5,62 @@ var bankAccount = 100;
 var purchased = false;
 var ethHolding = 0;
 
+function calculateSMA(data){
+	var holderSMA = 0;
+	var SMA = 0;
+
+	for(var i = data.length - 1; i > data.length - 11; i--){
+		holderSMA += data[i][4];
+	}
+	
+	return SMA = holderSMA / 10;
+}
+
+function calculateEMA(data){
+	console.log("SMA:" +  calculateSMA(data));
+}
+
 function getEth(){
 
 	publicClient.getProductHistoricRates('ETH-USD', { granularity: 60 }, function(error, response, data){
 		if(error){
 			console.log("error");
 		} else {
+			
 			var retrievedData = 0;
+
 			for(var i = 0; i < data.length; i++){
 				retrievedData += data[i][4];
 			}
+
 			var average_price = retrievedData / i;
 			var current_price = data[0][4];
+
 			if(current_price < average_price - (average_price * 0.025) && !purchased && bankAccount > 0){
 				ethHolding = bankAccount / current_price;
 				purchased = true;
+				bankAccount = 0;
+
 				console.log("bought");
-				console.log("Current Price:" + current_price);
 				console.log("Average Price: " + (average_price - (average_price * 0.025)).toString());
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
 			}
+
 			else if(purchased && current_price > average_price){
 				money_made = ethHolding * current_price;
 				bankAccount += money_made;
 				purchased = false;
+
 				console.log("sold");
-				console.log("Current Price:" + current_price);
 				console.log("Average Price: " + (average_price - (average_price * 0.025)).toString());
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
 			}
+
 		 	else {
+		 		calculateEMA(data);
 				console.log("not purchased");
-				console.log("Current Price:" + current_price);
 				console.log("Average Price: " + (average_price - (average_price * 0.025)).toString());
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
@@ -46,5 +68,6 @@ function getEth(){
 		}
 	})
 }
+
 
 setInterval(getEth, 5000);
