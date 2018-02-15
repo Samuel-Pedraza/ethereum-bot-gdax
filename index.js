84d1fc72ed2798f587d99c9ab66390cb1ad83e9d
Samuel Pedraza <samdpedraza@gmail.com>
2018-02-15 13:35:45 -0800

    some work left to do

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var bankAccount = 100;
 var purchased = false;
 var ethHolding = 0;
 var purchasedAtEMA = 0;
-var buys = 0;
-var sells = 0;
+var trades = 0;
+var purchasedAtPrice = 0;
+var previousBankAccountBalance = 0;
+var successfultrades = 0;
 
 function calculateSMA(data){
 	var holderSMA = 0;
@@ -15,12 +17,12 @@ function calculateSMA(data){
 	for(var i = data.length - 1; i > data.length - 11; i--){
 		holderSMA += data[i][4];
 	}
-	
+
 	return SMA = holderSMA / 10;
 }
 
 function calculateEMA(data){
-	var multiplier = 0.1818;	
+	var multiplier = 0.1818;
 	var lastEMA;
 	var currentEMA;
 
@@ -41,7 +43,7 @@ function getEth(){
 		if(error){
 			console.log("error");
 		} else {
-			
+
 			var retrievedData = 0;
 
 			for(var i = 0; i < data.length; i++){
@@ -51,49 +53,60 @@ function getEth(){
 			var average_price = retrievedData / i;
 			var current_price = data[0][4];
 
-			if(current_price < (calculateEMA(data)) && !purchased && bankAccount > 0){
+			if(current_price < (calculateEMA(data) - (calculateEMA(data) * 0.001)) && !purchased && bankAccount > 0){
 				ethHolding = bankAccount / current_price;
 				purchased = true;
+				previousBankAccountBalance = bankAccount;
 				bankAccount = 0;
-				purchasedAtEMA = calculateEMA(data);
-				buys += 1;
+				console.log("Purchase At: " + (calculateEMA(data) - (calculateEMA(data) * 0.001)))
+				purchasedAtPrice = current_price;
 
 				console.log("bought");
-				console.log("EMA: " + calculateEMA(data))
+				console.log("Purchase At: " + (calculateEMA(data) - (calculateEMA(data) * 0.001)))
 				console.log("Current Price: " + current_price);
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
-				console.log("Buys: " + buy);
-				console.log("");
+
 
 			}
 
-			else if(purchased && (current_price > purchasedAtEMA) || (current_price === (purchasedAtEMA - (purchasedAtEMA * 0.015)))){
+			else if(purchased && (current_price > ((purchasedAtEMA + purchasedAtPrice) / 2)) || (current_price < (purchasedAtEMA - (purchasedAtEMA * 0.01)))){
 				money_made = ethHolding * current_price;
 				bankAccount += money_made;
 				purchased = false;
 				ethHolding = 0;
 				purchasedAtEMA = 0;
-				sell +=1;
+				trades += 1;
+				purchasedAtPrice = 0;
 
 				console.log("sold");
-				console.log("EMA: " + calculateEMA(data))
+				console.log("Purchase At: " + (calculateEMA(data) - (calculateEMA(data) * 0.001)))
 				console.log("Current Price: " + current_price);
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
-				console.log("Sells: " + sell);
+				console.log("Trades: " + trades);
+				if(bankAccount > previousBankAccountBalance){
+					console.log("previous Bank Account Balance: " + previousBankAccountBalance)
+					successfultrades += 1;
+					console.log("Successful Trades: " + successfultrades);
+				} else if (bankAccount < previousBankAccountBalance) {
+					console.log("previous Bank Account Balance: " + previousBankAccountBalance)
+					successfultrades -= 1;
+					console.log("Successful Trades: " + successfultrades);
+				}
 				console.log("");
 			}
 
 		 	else {
 				console.log("not purchased");
-				console.log("EMA: " + calculateEMA(data))
+				console.log("Purchase At: " + (calculateEMA(data) - (calculateEMA(data) * 0.001)))
 				console.log("Current Price: " + current_price);
 				console.log("Ethereum Holding: " + ethHolding);
 				console.log("Bank Account: " + bankAccount);
-				
-				if( purchasedAtEMA > 0){
+
+				if( purchasedAtEMA > 0 && purchasedAtPrice > 0){
 					console.log("purchasedAtEMA: " + purchasedAtEMA);
+					console.log("purchasedAtPrice: " + purchasedAtPrice);
 				}
 
 				console.log("");
